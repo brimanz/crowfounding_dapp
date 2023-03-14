5da5ddc78531fd98e66b5ccca01acc2603c8e8ce
jonathan briceno <brimanz2014@gmail.com>
2023-03-14 17:07:24 -0400

    creating campaign function

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -9,7 +9,7 @@ contract CrowdFunding {
         string description;
         string image;
         uint256 target;
-        uint256 deadlin;
+        uint256 deadline;
         uint256 ammountCollected;
         address[] donators;
         uint256[] donations; 
@@ -19,10 +19,31 @@ contract CrowdFunding {
 
     uint256 public numberOfCampaigns = 0;
     
-    function createCampaign (){
+    function createCampaign(address _owner, string memory _title, string memory _description, unin256 _target, unin256 _deadline, string memory _image) public returns(uint256) {
+
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+
+        require(campaign.deadline < block.timestamp, "The deadline should be date in the future.");
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign. deadline = _deadline;
+        campaign.ammountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
     }
 
-    function donateToCampaign (){
+    function donateToCampaign(){
     }
     
+    function getDonators(){
+    }
+
+    function getCampaign(){
+    }
 }
